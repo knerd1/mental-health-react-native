08829bc3c917209ee892cf9d4a7a3452fd0c7810
Ashutosh <ap9696446@gmail.com>
2025-04-11 02:49:32 +0530

    App structue with login and registration.

---
+++ install.sh
@@ -0,0 +1,22 @@
+npx create-expo mental-health 
+
+npx expo install nativewind tailwindcss@3.x
+npx expo install expo-dev-client  
+npx expo install @react-native-community/datetimepicker
+npx expo install expo-video
+npm install jotai
+npm install react-hook-form @hookform/resolvers zod
+
+npx expo install stream-chat-expo
+npx expo install expo-file-system expo-image-manipulator
+npx expo install expo-media-library expo-image-picker expo-document-picker expo-sharing
+
+npx expo install expo-secure-store
+npx expo install expo-build-properties
+npx expo install @stream-io/video-react-native-sdk
+npx expo install @stream-io/react-native-webrtc
+npx expo install @config-plugins/react-native-webrtc
+npx expo install react-native-incall-manager
+npx expo install react-native-svg
+npx expo install @react-native-community/netinfo
+npx expo install @notifee/react-native
